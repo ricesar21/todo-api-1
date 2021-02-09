46bb124e9bc3fe4eb3d5ec150066cb5051259ba1
Rodrigo Barreto <barretorodrigo@gmail.com>
2021-02-09 16:08:42 -0300

    lista de tarefas

--- index.js
+++ index.js
@@ -2,9 +2,46 @@ const express = require('express'); // framework
 const app = express(); 
 const port = 3000;
 const bodyParser = require('body-parser'); // interpretação do json
-
+const { Tarefa } = require('./models');
 
 app.use(bodyParser.json());
+
+app.get('/tarefas', async(req,res)=>{
+    const tarefas = await Tarefa.findAll();
+    res.status(200).json(tarefas);
+})
+
+app.get('/tarefas/:id', async(req, res)=>{
+    const tarefa = await Tarefa.findAll({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(tarefa);
+});
+
+app.post('/tarefas', async(req, res)=>{
+    const tarefa = await Tarefa.create(req.body);
+    res.status(201).json(tarefa);
+});
+
+app.delete('/tarefas/:id', async(req, res)=>{
+    const tarefa = await Tarefa.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200).json(tarefa);
+});
+
+app.put('/tarefas/:id', async(req, res)=>{
+    const tarefa = await Tarefa.update(req.body, {
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(tarefa);  // V1NNI7#0001
+});
 
 
 app.listen(port);

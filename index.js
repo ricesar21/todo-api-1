624784fe58d9b9c4a23d759fd198dc69ca89a64c
ricesar21 <ricesar21@gmail.com>
2021-02-23 02:25:46 -0300

    commit

--- index.js
+++ index.js
@@ -1,54 +1,69 @@
-const express = require('express'); // framework
-const app = express(); 
-const port = 3000;
-const bodyParser = require('body-parser'); // interpretação do json
+const express = require('express');
+const app = express();
+const port = 3000
+const bodyParser = require('body-parser')
 const { Tarefa } = require('./models');
-const cors = require('cors');
 
 app.use(bodyParser.json());
-//app.use(cors());
 
-app.get('/', async(req, res)=>{
-    console.log('Running')
-    res.send('Running =)');
-})
+app.get('/', async (req, res) => {
+    res.send('running :P');
+});
 
-app.get('/tarefas', async(req,res)=>{
+app.get('/tarefas', async (req, res) => {
     const tarefas = await Tarefa.findAll();
     res.status(200).json(tarefas);
-})
+});
 
-app.get('/tarefas/:id', async(req, res)=>{
+app.get('/tarefas/:id', async (req, res) => {
     const tarefa = await Tarefa.findAll({
-        where:{
+        where: {
             id: req.params.id
         }
     });
-    res.status(200).json(tarefa);
+    res.status(200).json(tarefa)
 });
 
-app.post('/tarefas', async(req, res)=>{
+app.post('/tarefas', async (req, res) => {
     const tarefa = await Tarefa.create(req.body);
     res.status(201).json(tarefa);
 });
 
-app.delete('/tarefas/:id', async(req, res)=>{
+app.delete('/tarefas/:id', async (req, res) => {
     const tarefa = await Tarefa.destroy({
-        where:{
-            id: req.params.id
-        }
-    })
-    res.status(200).json(tarefa);
-});
-
-app.put('/tarefas/:id', async(req, res)=>{
-    const tarefa = await Tarefa.update(req.body, {
-        where:{
+        where: {
             id: req.params.id
         }
     });
-    res.status(200).json(tarefa);  // V1NNI7#0001
+    res.status(200).json(tarefa)
 });
 
+app.put('/tarefas/:id', async (req, res) => {
+    const tarefa = await Tarefa.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(tarefa);
+});
+
+app.get('/tarefas/status/:status', async (req, res) => {
+    const status = req.params.status !== "false"
+    const tarefas = await Tarefa.findAll({
+        where: {
+            status: status
+        }
+    });
+    res.status(200).json(tarefas);
+});
+
+app.get('/tarefas/prioridade/:prioridade', async (req, res) => {
+    const tarefas = await Tarefa.findAll({
+        where: {
+            prioridade: req.params.prioridade
+        }
+    });
+    res.status(200).json(tarefas);
+});
 
 app.listen(process.env.PORT || port);
